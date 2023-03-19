58695d207fae3442641e762a54860b3116caa74f
rcs04aaa <122098552+rcs04aaa@users.noreply.github.com>
2023-03-19 09:46:23 +0000

    [更新]レイアウト

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -6,6 +6,7 @@ class Public::PostsController < ApplicationController
   
   def index
     @posts = Post.all
+    @total = 0
   end
   
   def show
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,25 +1,33 @@
 class Public::UsersController < ApplicationController
+
   def show
     # @posts = @user.posts
     @user = current_user
+    @total = 0
   end
-  
+
   def edit
     @user = current_user
   end
-  
+
   def update
     @user = current_user
-    if @user.update
-      redirect_to user_path(current_user), notice: "更新が完了しました"
+    if @user.update(user_params)
+      redirect_to users_my_page_path(current_user), notice: "更新が完了しました"
     else
       render :edit
     end
   end
-  
+
   def favorites
     @user = User.find(params[:id])
     @favorite_posts = Post.find(favorites)
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :email, :is_deleted)
+  end
+
 end
